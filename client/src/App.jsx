249d070368ce2d9f5de3d27c5a71b13b05689e45
PRATIK <pratikh004@gmail.com>
2025-07-02 13:58:48 +0530

    bg image fixed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ import { AuthContext } from '../context/authContext.jsx'
 const App = () => {
   const {authUser} = useContext(AuthContext) 
   return (
-    <div className='bgbgimage.jpg")]'>
+    <div style={{ backgroundImage: `url("/public/bgimage.jpg")` }}>
       <Toaster/>
       <Routes>
         <Route path='/' element={authUser ? <HomePage /> : <Navigate to='/login' />} />
--- server/server.js
+++ server/server.js
@@ -59,7 +59,7 @@ app.use("/api/messages", messageRouter);
 await connectDB();
 
 if(process.env.NODE_ENV === "production") {
-    const PORT = process.env.PORT;
+    const PORT = process.env.PORT || 5000;
     server.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
